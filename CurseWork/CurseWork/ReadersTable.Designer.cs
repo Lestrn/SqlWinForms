71f5d1bbbe950eee7d08938a0b9f602effaf76f4
Lestrn <ilestrn@gmail.com>
2022-11-17 18:51:09 +0200

    Readers Logic almost end

--- CurseWork/CurseWork/ReadersTable.Designer.cs
+++ CurseWork/CurseWork/ReadersTable.Designer.cs
@@ -36,18 +36,23 @@
             this.FIOTextBox = new System.Windows.Forms.TextBox();
             this.AdressTextBox = new System.Windows.Forms.TextBox();
             this.PhoneTextBox = new System.Windows.Forms.TextBox();
-            this.NumberReaderTextBox = new System.Windows.Forms.TextBox();
-            this.listView1 = new System.Windows.Forms.ListView();
+            this.ReadersListView = new System.Windows.Forms.ListView();
             this.NumberColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.PIBColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.AddressColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.PhoneColumn = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.AddButton = new System.Windows.Forms.Button();
+            this.DeleteButton = new System.Windows.Forms.Button();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.NumberReaderComboBox = new System.Windows.Forms.ComboBox();
+            this.ForAddTextBox = new System.Windows.Forms.TextBox();
             this.ReaderGroupBox.SuspendLayout();
             this.SuspendLayout();
             // 
             // ReaderGroupBox
             // 
-            this.ReaderGroupBox.Controls.Add(this.NumberReaderTextBox);
+            this.ReaderGroupBox.Controls.Add(this.ForAddTextBox);
+            this.ReaderGroupBox.Controls.Add(this.NumberReaderComboBox);
             this.ReaderGroupBox.Controls.Add(this.PhoneTextBox);
             this.ReaderGroupBox.Controls.Add(this.AdressTextBox);
             this.ReaderGroupBox.Controls.Add(this.PhoneLabel);
@@ -94,7 +99,7 @@
             // FIOLabel
             // 
             this.FIOLabel.AutoSize = true;
-            this.FIOLabel.Location = new System.Drawing.Point(6, 68);
+            this.FIOLabel.Location = new System.Drawing.Point(6, 99);
             this.FIOLabel.Name = "FIOLabel";
             this.FIOLabel.Size = new System.Drawing.Size(44, 16);
             this.FIOLabel.TabIndex = 2;
@@ -102,7 +107,7 @@
             // 
             // FIOTextBox
             // 
-            this.FIOTextBox.Location = new System.Drawing.Point(142, 62);
+            this.FIOTextBox.Location = new System.Drawing.Point(142, 99);
             this.FIOTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.FIOTextBox.Name = "FIOTextBox";
             this.FIOTextBox.Size = new System.Drawing.Size(95, 22);
@@ -124,38 +129,81 @@
             this.PhoneTextBox.Size = new System.Drawing.Size(95, 22);
             this.PhoneTextBox.TabIndex = 7;
             // 
-            // NumberReaderTextBox
+            // ReadersListView
             // 
-            this.NumberReaderTextBox.Location = new System.Drawing.Point(238, 22);
-            this.NumberReaderTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.NumberReaderTextBox.Name = "NumberReaderTextBox";
-            this.NumberReaderTextBox.Size = new System.Drawing.Size(95, 22);
-            this.NumberReaderTextBox.TabIndex = 8;
-            // 
-            // listView1
-            // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ReadersListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.NumberColumn,
             this.PIBColumn,
             this.AddressColumn,
             this.PhoneColumn});
-            this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(370, 12);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(396, 219);
-            this.listView1.TabIndex = 6;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
+            this.ReadersListView.HideSelection = false;
+            this.ReadersListView.Location = new System.Drawing.Point(370, 12);
+            this.ReadersListView.Name = "ReadersListView";
+            this.ReadersListView.Size = new System.Drawing.Size(396, 219);
+            this.ReadersListView.TabIndex = 6;
+            this.ReadersListView.UseCompatibleStateImageBehavior = false;
+            this.ReadersListView.View = System.Windows.Forms.View.Details;
+            this.ReadersListView.SelectedIndexChanged += new System.EventHandler(this.SelectedItem);
+            // 
+            // AddButton
+            // 
+            this.AddButton.Location = new System.Drawing.Point(380, 256);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(104, 23);
+            this.AddButton.TabIndex = 7;
+            this.AddButton.Text = "Добавить";
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // DeleteButton
+            // 
+            this.DeleteButton.Location = new System.Drawing.Point(509, 256);
+            this.DeleteButton.Name = "DeleteButton";
+            this.DeleteButton.Size = new System.Drawing.Size(110, 23);
+            this.DeleteButton.TabIndex = 8;
+            this.DeleteButton.Text = "Удалить";
+            this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
+            // 
+            // EditButton
+            // 
+            this.EditButton.Location = new System.Drawing.Point(642, 256);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(105, 23);
+            this.EditButton.TabIndex = 9;
+            this.EditButton.Text = "Изменить";
+            this.EditButton.UseVisualStyleBackColor = true;
+            this.EditButton.Click += new System.EventHandler(this.EditButton_Click);
+            // 
+            // NumberReaderComboBox
+            // 
+            this.NumberReaderComboBox.FormattingEnabled = true;
+            this.NumberReaderComboBox.Location = new System.Drawing.Point(212, 25);
+            this.NumberReaderComboBox.Name = "NumberReaderComboBox";
+            this.NumberReaderComboBox.Size = new System.Drawing.Size(121, 24);
+            this.NumberReaderComboBox.TabIndex = 10;
+            // 
+            // ForAddTextBox
+            // 
+            this.ForAddTextBox.Location = new System.Drawing.Point(212, 54);
+            this.ForAddTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.ForAddTextBox.Name = "ForAddTextBox";
+            this.ForAddTextBox.Size = new System.Drawing.Size(95, 22);
+            this.ForAddTextBox.TabIndex = 11;
             // 
             // ReadersTable
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.EditButton);
+            this.Controls.Add(this.DeleteButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.ReadersListView);
             this.Controls.Add(this.ReaderGroupBox);
             this.Name = "ReadersTable";
             this.Text = "ReadersTable";
+            this.Load += new System.EventHandler(this.ReadersLoad);
             this.ReaderGroupBox.ResumeLayout(false);
             this.ReaderGroupBox.PerformLayout();
             this.ResumeLayout(false);
@@ -171,12 +219,16 @@
         private System.Windows.Forms.Label AdressLabel;
         private System.Windows.Forms.Label NumberReaderLabel;
         private System.Windows.Forms.TextBox AdressTextBox;
-        private System.Windows.Forms.TextBox NumberReaderTextBox;
         private System.Windows.Forms.TextBox PhoneTextBox;
-        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView ReadersListView;
         private System.Windows.Forms.ColumnHeader NumberColumn;
         private System.Windows.Forms.ColumnHeader PIBColumn;
         private System.Windows.Forms.ColumnHeader AddressColumn;
         private System.Windows.Forms.ColumnHeader PhoneColumn;
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.Button DeleteButton;
+        private System.Windows.Forms.Button EditButton;
+        private System.Windows.Forms.ComboBox NumberReaderComboBox;
+        private System.Windows.Forms.TextBox ForAddTextBox;
     }
 }
