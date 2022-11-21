907ff57cfd314328a171d0303347063563c528de
Lestrn <ilestrn@gmail.com>
2022-11-21 22:51:49 +0200

    fixed some queries

--- CurseWork/CurseWork/Query.cs
+++ CurseWork/CurseWork/Query.cs
@@ -15,16 +15,16 @@ namespace CurseWork
     {
         private string selectorQuery = "SELECT * FROM Видавництво WHERE Код = 232";
         private string proectionQuery = "SELECT Бібліотека.Код_УДК, Бібліотека.Назва_книги FROM Бібліотека INNER JOIN Журнал ON Бібліотека.Код_УДК = Журнал.Код_УДК";
-        private string connectionWithSelectorQuery = "SELECT Бібліотека.Назва_книги, Автори.ПІБ AS Автор_книги FROM Бібліотека INNER JOIN Автори ON Бібліотека.Код_автора_книги = Автори.Код_автора WHERE Автори.Код_автора = 44";
-        private string connectionWithProectionQuery = "SELECT Бібліотека.Код_видавництва, Видавництво.Видавництво AS FROM Бібліотека INNER JOIN Видавництво ON Бібліотека.Код_видавництва = Видавництво.Код";
-        private string groupingWithoutСonditionQuery = "SELECT Автори.ФІО AS Автор_книги, COUNT(Бібліотека.Назва_книги) AS Кількість_написаних_книг FROM Бібліотека INNER JOIN Автори ON Бібліотека.Код_автора_книги = Автори.Код_автора GROUP BY Автори.ПІБ";
-        private string groupingWithСonditionQuery = "SELECT Жанри.Жанр, COUNT(Бібліотека.Назва_книги) AS Кількість_книг FROM Бібліотека INNER JOIN Жанри ON Бібліотека.Код_жанру = Жанри.Код_жанра WHERE  Жанр = ‘роман’ GROUP BY Бібліотека.Назва_книги";
+        private string connectionWithSelectorQuery = "SELECT Бібліотека.Назва_книги, Автори.ФІО AS Автор_книги FROM Бібліотека INNER JOIN Автори ON Бібліотека.Код_автора_книги = Автори.Код_автора WHERE Автори.Код_автора = 44";
+        private string connectionWithProectionQuery = "SELECT Бібліотека.Код_видавництва, Видавництво.Видавництво FROM Бібліотека INNER JOIN Видавництво ON Бібліотека.Код_видавництва = Видавництво.Код";
+        private string groupingWithoutСonditionQuery = "SELECT Автори.ФІО AS Автор_книги, COUNT(Бібліотека.Назва_книги) AS Кількість_написаних_книг FROM Бібліотека INNER JOIN Автори ON Бібліотека.Код_автора_книги = Автори.Код_автора GROUP BY Автори.ФІО";
+        private string groupingWithСonditionQuery = "SELECT Бібліотека.Назва_книги, COUNT(Жанри.Жанр) AS Кількість_книг FROM Бібліотека INNER JOIN Жанри ON Бібліотека.Код_жанру=Жанри.Код_жанра WHERE  Жанр=\"роман\" GROUP BY Бібліотека.Назва_книги";
         private string agregationSelectQuery = "SELECT AVG(Ціна) AS Середня_ціна FROM Бібліотека";
         private string agregationHavingQuery = "SELECT Назва_книги, MIN(Рік_видання) AS  Рік_видання FROM Бібліотека GROUP BY Назва_книги HAVING MIN(Рік_видання)< 2000";
         private string crossQuery = "SELECT Номер_читацького_квитка FROM Журнал INTERSECT SELECT Номер_читацького_квитка, ПІБ_читача, Номер_телефона_читача FROM Читачі";
         private string uniteQuery = "SELECT ПІБ_читача FROM Читачі UNION SELECT Адреса_читача FROM Читачі";
         private string substructionQuery = "SELECT * FROM Читачі EXCEPT SELECT Номер_читацького_квитка FROM Журнал";
-        private string divisionQuery = "SELECT * FROM Журнал WHERE Статус = ‘заброньована’ AND Статус = ‘взята на прочитання’ GROUP BY Номер_читацького_квитка";
+        private string divisionQuery = "SELECT Номер_читацького_квитка FROM Журнал WHERE Статус = ‘заброньована’ AND Статус = ‘взята на прочитання’ GROUP BY Номер_читацького_квитка";
         public Query(string selectedQuery)
         {
             InitializeComponent();
@@ -73,8 +73,8 @@ namespace CurseWork
                     FormService.UpdateListViewWithDB(QueryListView, DbConnection, groupingWithoutСonditionQuery, 2);
                     break;
                 case "Об'єднання":
-                    FormService.RebuildListView(QueryListView, "ПІБ читача", "Адреса читача"); //+
-                    FormService.UpdateListViewWithDB(QueryListView, DbConnection, uniteQuery, 2);
+                    FormService.RebuildListView(QueryListView, "ПІБ_Адреса"); //+
+                    FormService.UpdateListViewWithDB(QueryListView, DbConnection, uniteQuery, 1);
                     break;
                 case "Перетин":
                     FormService.RebuildListView(QueryListView,  "Номер читацького квитка"); //+
