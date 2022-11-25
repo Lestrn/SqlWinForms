44e136b157576b9460d3cb1931e9bac8475fba3a
Lestrn <ilestrn@gmail.com>
2022-11-26 00:26:06 +0200

    Excel Report DONE WORKING GG WP WE DONE YAYAY only cleaning left

--- CurseWork/CurseWork/Report.Designer.cs
+++ CurseWork/CurseWork/Report.Designer.cs
@@ -29,56 +29,157 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
-            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
-            this.libraryDB = new CurseWork.LibraryDB();
             this.libraryDBBindingSource = new System.Windows.Forms.BindingSource(this.components);
-            ((System.ComponentModel.ISupportInitialize)(this.libraryDB)).BeginInit();
+            this.libraryDB = new CurseWork.LibraryDB();
+            this.PrintBtn = new System.Windows.Forms.Button();
+            this.LibraryListView = new System.Windows.Forms.ListView();
+            this.BookName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Author = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DayOut = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Genre = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Photo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Code = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.PublishName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Price = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Amount = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.IsNewDayOut = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Description = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.KeyWords = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             ((System.ComponentModel.ISupportInitialize)(this.libraryDBBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libraryDB)).BeginInit();
             this.SuspendLayout();
-            // 
-            // reportViewer1
-            // 
-            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            reportDataSource1.Name = "DataSet1";
-            reportDataSource1.Value = this.libraryDBBindingSource;
-            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
-            this.reportViewer1.LocalReport.ReportEmbeddedResource = "CurseWork.LibraryReport.rdlc";
-            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
-            this.reportViewer1.Name = "reportViewer1";
-            this.reportViewer1.ServerReport.BearerToken = null;
-            this.reportViewer1.Size = new System.Drawing.Size(800, 450);
-            this.reportViewer1.TabIndex = 0;
-            // 
-            // libraryDB
-            // 
-            this.libraryDB.DataSetName = "LibraryDB";
-            this.libraryDB.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
             // 
             // libraryDBBindingSource
             // 
             this.libraryDBBindingSource.DataSource = this.libraryDB;
             this.libraryDBBindingSource.Position = 0;
             // 
+            // libraryDB
+            // 
+            this.libraryDB.DataSetName = "LibraryDB";
+            this.libraryDB.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // PrintBtn
+            // 
+            this.PrintBtn.Location = new System.Drawing.Point(12, 21);
+            this.PrintBtn.Name = "PrintBtn";
+            this.PrintBtn.Size = new System.Drawing.Size(776, 31);
+            this.PrintBtn.TabIndex = 1;
+            this.PrintBtn.Text = "Print";
+            this.PrintBtn.UseVisualStyleBackColor = true;
+            this.PrintBtn.Click += new System.EventHandler(this.PrintBtn_Click);
+            // 
+            // LibraryListView
+            // 
+            this.LibraryListView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.BookName,
+            this.Author,
+            this.DayOut,
+            this.Genre,
+            this.Photo,
+            this.Code,
+            this.PublishName,
+            this.Price,
+            this.Amount,
+            this.IsNewDayOut,
+            this.Description,
+            this.KeyWords});
+            this.LibraryListView.HideSelection = false;
+            this.LibraryListView.Location = new System.Drawing.Point(13, 57);
+            this.LibraryListView.Margin = new System.Windows.Forms.Padding(4);
+            this.LibraryListView.Name = "LibraryListView";
+            this.LibraryListView.Size = new System.Drawing.Size(775, 398);
+            this.LibraryListView.TabIndex = 4;
+            this.LibraryListView.UseCompatibleStateImageBehavior = false;
+            this.LibraryListView.View = System.Windows.Forms.View.Details;
+            // 
+            // BookName
+            // 
+            this.BookName.Text = "Назва книги";
+            // 
+            // Author
+            // 
+            this.Author.Text = "Автор";
+            // 
+            // DayOut
+            // 
+            this.DayOut.Text = "Рік видавництва";
+            // 
+            // Genre
+            // 
+            this.Genre.Text = "Жанр";
+            // 
+            // Photo
+            // 
+            this.Photo.Text = "Обкладинка";
+            // 
+            // Code
+            // 
+            this.Code.Text = "Код УДК";
+            // 
+            // PublishName
+            // 
+            this.PublishName.DisplayIndex = 11;
+            this.PublishName.Text = "Видавництво";
+            // 
+            // Price
+            // 
+            this.Price.DisplayIndex = 6;
+            this.Price.Text = "Ціна";
+            // 
+            // Amount
+            // 
+            this.Amount.DisplayIndex = 7;
+            this.Amount.Text = "Кількість";
+            // 
+            // IsNewDayOut
+            // 
+            this.IsNewDayOut.DisplayIndex = 8;
+            this.IsNewDayOut.Text = "Нове видавництво";
+            // 
+            // Description
+            // 
+            this.Description.DisplayIndex = 9;
+            this.Description.Text = "Опис";
+            // 
+            // KeyWords
+            // 
+            this.KeyWords.DisplayIndex = 10;
+            this.KeyWords.Text = "Ключові слава";
+            // 
             // Report
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.LibraryListView);
+            this.Controls.Add(this.PrintBtn);
             this.Name = "Report";
             this.Text = "Report";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Report_FormClosed);
             this.Load += new System.EventHandler(this.Report_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.libraryDB)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.libraryDBBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libraryDB)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
         private System.Windows.Forms.BindingSource libraryDBBindingSource;
         private LibraryDB libraryDB;
+        private System.Windows.Forms.Button PrintBtn;
+        private System.Windows.Forms.ListView LibraryListView;
+        private System.Windows.Forms.ColumnHeader BookName;
+        private System.Windows.Forms.ColumnHeader Author;
+        private System.Windows.Forms.ColumnHeader DayOut;
+        private System.Windows.Forms.ColumnHeader Genre;
+        private System.Windows.Forms.ColumnHeader Photo;
+        private System.Windows.Forms.ColumnHeader Code;
+        private System.Windows.Forms.ColumnHeader PublishName;
+        private System.Windows.Forms.ColumnHeader Price;
+        private System.Windows.Forms.ColumnHeader Amount;
+        private System.Windows.Forms.ColumnHeader IsNewDayOut;
+        private System.Windows.Forms.ColumnHeader Description;
+        private System.Windows.Forms.ColumnHeader KeyWords;
     }
 }
--- CurseWork/CurseWork/Report.cs
+++ CurseWork/CurseWork/Report.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿
+using Microsoft.Office.Interop.Excel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,14 +16,99 @@ namespace CurseWork
 {
     public partial class Report : Form
     {
+
         public Report()
         {
             InitializeComponent();
+            DbConnection = new OleDbConnection(DataBaseController.DatabaseConnection);
+            DbConnection.Open();
         }
+
+        public OleDbConnection DbConnection { get; }
 
         private void Report_Load(object sender, EventArgs e)
         {
-            reportViewer1.RefreshReport();
+            LibraryListView.FullRowSelect = true;
+            FormService.UpdateListViewWithDB(LibraryListView, DbConnection, "SELECT * FROM Бібліотека", 12);
+        }
+
+        private void Report_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            DbConnection.Close();
+        }
+
+        private void PrintBtn_Click(object sender, EventArgs e)
+        {
+            ToExcel();
+        }
+        //void Printer()
+        //{
+        //    PrintDocument PrintDocument = new PrintDocument();
+        //    PrintDocument.PrintPage += (object sender, PrintPageEventArgs e) =>
+        //    {
+        //        Font font = new Font("Arial", 12);
+        //        float offset = e.MarginBounds.Top;
+        //        foreach (ListViewItem Item in LibraryListView.Items)
+        //        {
+        //            for (int i = 0; i < Item.SubItems.Count - 1; i++)
+        //            {
+        //                offset += (font.GetHeight() + 5.0f);
+        //                PointF location = new System.Drawing.PointF(e.MarginBounds.Left, offset);
+        //                e.Graphics.DrawString(Item.SubItems[i].Text, font, Brushes.Black, location);
+        //            }
+        //            // The 5.0f is to add a small space between lines
+
+        //        }
+        //    };
+        //    PrintDocument.Print();
+        //    //foreach (ListViewItem itemRow in LibraryListView.Items)
+        //    //{
+        //    //    int i = 0;
+        //    //    for (i = 0; i < itemRow.SubItems.Count - 1; i++)
+        //    //    {
+        //    //        pdfTable.AddCell(itemRow.SubItems[i].Text);
+        //    //    }
+        //    //}
+
+        //}
+        public void ToExcel()
+        {
+            using(SaveFileDialog sfd = new SaveFileDialog() { Filter = "Excel Workbook|*.xls", ValidateNames = true})
+            {
+                if(sfd.ShowDialog() == DialogResult.OK)
+                {
+                    Microsoft.Office.Interop.Excel.Application application = new Microsoft.Office.Interop.Excel.Application();
+                    Workbook workbook = application.Workbooks.Add(XlSheetType.xlWorksheet);
+                    Worksheet worksheet = (Worksheet)application.ActiveSheet;
+                    application.Visible = false;
+                    worksheet.Cells[1, 1] = "Назва книги";
+                    worksheet.Cells[1, 2] = "Код автора";
+                    worksheet.Cells[1, 3] = "Рік видання";
+                    worksheet.Cells[1, 4] = "Код жанру";
+                    worksheet.Cells[1, 5] = "Обкладенка";
+                    worksheet.Cells[1, 6] = "Код УДК";
+                    worksheet.Cells[1, 7] = "Код Видавництва";
+                    worksheet.Cells[1, 8] = "Ціна";
+                    worksheet.Cells[1, 9] = "Кількість у бібліотеці";
+                    worksheet.Cells[1, 10] = "Че є новим виданням";
+                    worksheet.Cells[1, 11] = "Примітки";
+                    worksheet.Cells[1, 12] = "Ключові слова";
+                    int i = 2;
+                    foreach (ListViewItem item in LibraryListView.Items)
+                    {
+                        for (int j = 1, h = 0; j <= 12; j++, h++)
+                        {
+                            worksheet.Cells[i, j] = item.SubItems[h].Text;
+                        }
+                        i++;
+                    }
+                    workbook.SaveAs(sfd.FileName, XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing, true, false, XlSaveAsAccessMode.xlNoChange, XlSaveConflictResolution.xlLocalSessionChanges, Type.Missing, Type.Missing);
+                    application.Quit();
+                    MessageBox.Show("VSE AHUENNO");
+                }
+            }
         }
     }
+  
 }
+
