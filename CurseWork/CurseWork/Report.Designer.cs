8dededd30513732e9963f5248b77203c5ef98ad2
Lestrn <ilestrn@gmail.com>
2022-11-25 20:55:14 +0200

    Almost done report

--- CurseWork/CurseWork/Report.Designer.cs
+++ CurseWork/CurseWork/Report.Designer.cs
@@ -28,17 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
             this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.libraryDB = new CurseWork.LibraryDB();
+            this.libraryDBBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.libraryDB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libraryDBBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // reportViewer1
             // 
             this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.libraryDBBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "CurseWork.LibraryReport.rdlc";
             this.reportViewer1.Location = new System.Drawing.Point(0, 0);
             this.reportViewer1.Name = "reportViewer1";
             this.reportViewer1.ServerReport.BearerToken = null;
             this.reportViewer1.Size = new System.Drawing.Size(800, 450);
             this.reportViewer1.TabIndex = 0;
+            // 
+            // libraryDB
+            // 
+            this.libraryDB.DataSetName = "LibraryDB";
+            this.libraryDB.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // libraryDBBindingSource
+            // 
+            this.libraryDBBindingSource.DataSource = this.libraryDB;
+            this.libraryDBBindingSource.Position = 0;
             // 
             // Report
             // 
@@ -49,6 +69,8 @@
             this.Name = "Report";
             this.Text = "Report";
             this.Load += new System.EventHandler(this.Report_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.libraryDB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.libraryDBBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,5 +78,7 @@
         #endregion
 
         private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource libraryDBBindingSource;
+        private LibraryDB libraryDB;
     }
 }
